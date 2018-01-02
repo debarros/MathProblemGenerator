01f99de4b50c1678c8327626b7faea45f6377c42
debarros <pj.debarros@gmail.com>
2018-01-01 22:09:37 -0500

    Reorganized some code and added some comments

--- create rational equations.R
+++ create rational equations.R
@@ -1,69 +1,59 @@
 #Beastly Rational Equation Problem Creator
 
-#This code will allow you to create problems that result in the form (x^2 - a^2b^2)(x + c)=0, 
-#and can be rewritten as  x^3 + (c+1)x + long*x = x^2 + medium*x + large
-# or as x(x+q)(x+r) = (x + fac1)(x + fac2)
+# This code will allow you to create problems that result in the form (x^2 - a^2b^2)(x + d)=0, [form 1]
+# and can be rewritten as  x^3 + (d+1)x + long*x = x^2 + medium*x + large [form 2]
+# or as x(x+q)(x+r) = (x + fac1)(x + fac2) [form 3]
 # which can then be reorganized and unsimplified as a rational equation 
 
 
 q <- 1
 a <- 1
 b <- 1
-c <- 1
+d <- 1
 r <- 1
 
 M <- data.frame(0,0,0,0,0,0,9)
 M
-names(M) <- c("a","b","c","q","r","large", "mag")
+names(M) <- c("a","b","d","q","r","large", "mag")
 
-i <- 1
+i <- 1 # initialize the row of the matrix
 
+q.range = c(-30:-1, 1:30)
+a.range = 2:30
 M[i,]
 
-for (q in c(-30:30)){
-  for (r in c(q:30)){
-    c <- q + r - 1
-    for (a in c(2:30)){
-      if ((q*r-a*c)/(a-a^2)>0){
-        b <- sqrt((q*r-a*c)/(a-a^2))
-        if((round(b)==b) && (b^2 != abs(b))){
-          if ((b < 100000) && (abs(c) != abs(b)) && (abs(b) != abs(a)) && (abs(c) != abs(a))){
-            M[i,] <- c(a, b, c, q, r, a^2*b^2*c,abs(a^2*b^2*c))
-            i <- i+1
-          }
-        }  
-      }
-    }
-  }
-}
-M2 <- M[c(which(M$mag != 0)),]
-M2 <- M2[c(which(M2$r != 0)),]
-M2 <- M2[c(which(M2$q != 0)),]
-M2$long = -M2$a^2 * M2$b^2 + M2$a * M2$b^2 + M2$a * M2$c
-M2$medium = M2$a * M2$b^2 + M2$a * M2$c
-M2$fac1 = M2$a*M2$b^2
-M2$fac2 = M2$a*M2$c
-  
-which(M2$fac2==0)
+for(q in q.range){
+  r.range = q:30
+  r.range = r.range[r.range != 0]     # don't want r to be 0
+  r.range = r.range[r.range != 1 - q] # don't want d to end up being 0
+  for(r in r.range){
+    d <- q + r - 1                    # calculate d
+    for (a in a.range){
+      if((q*r - a*d)/(a - a^2) > 0){              # don't want b to end up being a complex number
+        b <- sqrt((q*r - a*d)/(a - a^2))          # calculate b
+        if((round(b) == b) & (b^2 != abs(b))){    # if b is an integer other than -1, 0, or 1
+          if ((b < 100000) & length(unique(abs(c(a,b,d)))) == 3){ # if b is not too large, and a, b, and d are all different
+            large = a^2*b^2*d                     # calculate large
+            mag = abs(a^2*b^2*d)                  # calculate the magnitude
+            M[i,] <- c(a, b, d, q, r, large, mag) # add it to the set
+            i <- i + 1                            # move to the next row
+          } # /if b is not too large and a, b, and d are all different
+        } # /if b is an integer other than -1, 0, or 1
+      } # /if b is not complex
+    } # /for each value of a
+  } # /for each value of r
+} # /for each value of q
 
-str(M2)
+M2 <- M[M$mag != 0,]
 
-grab <- which(M2$mag == min(M2$mag))
-M2[grab,]
-
-which(M$q == M$r)
+M2$long = -M2$a^2 * M2$b^2 + M2$a * M2$b^2 + M2$a * M2$d # calculate long
+M2$medium = M2$a * M2$b^2 + M2$a * M2$d                  # calculate medium
+M2$fac1 = M2$a*M2$b^2                                    # calculate fac1
+M2$fac2 = M2$a*M2$d                                      # calculate fac2
 
 
-a = M2[min(grab),1]
-b = M2[min(grab),2]
-c = M2[min(grab),3]
-q = M2[min(grab),4]
-r = M2[min(grab),5]
-long = -a^2*b^2+a*b^2+a*c
-medium = a*b^2+a*c
-large = a^2*b^2*c
-fac1 = a*b^2
-fac2 = a*c
 
 
-write.csv(M2, file = "C:/Docs/GTH/2014-2015/Math dept/stuff.csv")
+
+
+
